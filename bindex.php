4887c1acaf5109e4746ec78e654f3f94a9a37d10
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2022-11-10 06:31:52 +0700

    Create bindex.php

---
+++ bindex.php
@@ -0,0 +1,213 @@
+<?php 
+session_start();
+require_once 'functions.php';
+dem_views();
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="cua nhom 450k">
+	<link rel="stylesheet" href="style.css">
+	<!-- <link rel="stylesheet" href="font/fontawesome/css/fontawesome.min.css"> -->
+	<script src="https://kit.fontawesome.com/77a8bfd2b1.js" crossorigin="anonymous"></script>
+	<title>Document</title>
+</head>
+<body>
+	<div class="app-A">
+		<div class="box">
+			<div class="icon icon-1" data-title="Theo doi tren FB">
+				<!-- <a href=""><i class="fa-brands fa-facebook-square"></i></a> -->
+				<div class="button">
+					<a href=""><!-- <i class="fa-brands fa-facebook"></i> -->
+						<i class="fa-brands fa-facebook-f"></i>
+
+					</a>
+			    </div>
+		    </div>
+				
+			<div class="icon icon-2" data-title="theo doi tren YT">
+				<a href=""><i class="fa-brands fa-youtube"></i></a>
+
+			</div>
+			<div class="icon icon-3" data-title="Call us">
+				<a href=""><i class="fa-solid fa-phone"></i> 
+			</div>
+
+		     <div class="icon icon-4" data-title="Nhom kinh buon ma thuot so 415 le duan">
+		     	<a href=""><i class="fa-solid fa-location-dot"></i> <span>Chi Duong</span> </a>
+				
+			</div>
+
+		</div>
+		<div class="header-search">
+			<div class="search">
+				<form action="">
+					<input type="text" placeholder="Nhap de tim kiem san pham">
+					<button><i class="fa-solid fa-magnifying-glass"></i></button>
+				</form>
+			</div>
+			<div class="dang-nhap">
+				<a href="">Dang nhap/Dang ky</a>
+
+			</div>
+			<div class="gio-hang">
+				<a href="">Gio hang/0d 
+		        <i class="fa-solid fa-bag-shopping"></i></a>
+			</div>
+		</div>
+		<div class="menu">
+			<div class="danhsach">
+				<i class="fa-solid fa-bars"></i>DANH SACH SAN PHAM
+			</div>
+			<div class="trang-chu">
+				<a href=""><i class="fa-solid fa-house"></i>TRANG CHU</a>
+			</div>
+			<div class="gioi-thieu">
+				<a href=""><i class="fa-solid fa-house-user"></i>GIOI THIEU</a>
+			</div>
+		</div>
+		<div class="san-pham">
+			<div class="tieu-de">DANH SACH SAN PHAM</div>
+			
+			
+		</div>
+		<div class="list-product">
+			<div class="sanpham sp1">
+				<div class="hinhanh">
+					<a href=""><img width="300" height="300" src="http://localhost/tu9/upload/anh1.jpg"></a>
+					<button><i class="fa-solid fa-star"></i></button>
+					<a href="" class="xem">xem san pham</a>
+				</div>
+
+				<p>NHOM KINH</p>
+				<a href=""> Tu bep tren</a>
+				<p>650,000 <span class="gia">đ</span></p>
+			</div>
+
+			<div class="sanpham sp2">
+				<div class="hinhanh">
+					<a href=""><img width="300" height="300" src="http://localhost/tu9/upload/anh2.jpg"></a>
+					<button><i class="fa-solid fa-star"></i></button>
+					<a href="" class="xem">xem san pham</a>
+				</div>
+				<p>NHOM KINH</p>
+				<a href="">Kinh cuong luc</a>
+				<p>Lien he nhan bao gia</p>
+			</div>
+
+			<div class="sanpham sp3">
+				<div class="hinhanh">
+					<a href=""><img width="300" height="300" src="http://localhost/tu9/upload/anh3.jpg"></a>
+					<button><i class="fa-solid fa-star"></i></button>
+					<a href="" class="xem">xem san pham</a>
+				</div>
+				<p>NHOM KINH</p>
+				<a href="">Kinh cuong luc</a>
+				<p>1,990,000 <span class="gia">đ</span></p>
+
+			</div>
+
+			<div class="sanpham sp4">
+				<div class="hinhanh">
+					<a href=""><img width="300" height="300" src="http://localhost/tu9/upload/anh4.jpg"></a>
+					<button><i class="fa-solid fa-star"></i></button>
+					<a href="" class="xem">xem san pham</a>
+				</div>
+				<p>NHOM KINH</p>
+				<a href="">Kinh cuong luc</a>
+				<p>550,000 <span class="gia">đ</span></p>
+			</div>
+
+			<div class="sanpham sp5">
+				<div class="hinhanh">
+					<a href=""><img width="300" height="300" src="http://localhost/tu9/upload/anh5.jpg"></a>
+					<button><i class="fa-solid fa-star"></i></button>
+					<a href="" class="xem">xem san pham</a>
+				</div>
+				<p>NHOM KINH</p>
+				<a href="">Kinh cuong luc</a>
+			</div>
+
+			<div class="sanpham sp6">
+				<div class="hinhanh">
+					<a href=""><img width="300" height="300" src="http://localhost/tu9/upload/anh6.jpg"></a>
+					<button><i class="fa-solid fa-star"></i></button>
+					<a href="" class="xem">xem san pham</a>
+				</div>
+				<p>NHOM KINH</p>
+				<a href="">Kinh cuong luc</a>
+				<p>250,000 <span class="gia">đ</span></p>
+			</div>
+
+		</div>
+		<div class="jiutup">
+			<div>
+				<iframe loading="lazy" title="Bể Cá kiêm tủ đựng giày dép tại Tủ Kệ Top Buôn Ma Thuột - Đăk Lăk | 2 trong 1 |" width="330" height="206" src="https://www.youtube.com/embed/RszR9lrZTm4?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+			</div>
+			<div>
+				<iframe width="330" height="206" src="https://www.youtube.com/embed/XSNR7PPeeV4" title="Cận Cảnh Tủ Bánh Mì, Tủ Bán Cơm, Tủ Bán Phở tại Nhôm Kính TUKETOP Buôn Ma Thuột - Đăk Lăk | SS06" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
+			<div>
+				<iframe width="330" height="206" src="https://www.youtube.com/embed/RszR9lrZTm4" title="Bể Cá kiêm tủ đựng giày dép | Nhôm kính Buôn Ma Thuột Đăk Lăk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
+		</div>
+		<div class="slider">
+			<div class="sld sld-1 first">
+				<i class="fa-solid fa-star"></i>
+				<i class="fa-solid fa-star"></i>
+				<i class="fa-solid fa-star"></i>
+				<i class="fa-regular fa-star"></i>
+				<i class="fa-regular fa-star"></i>
+				
+				<p>Nhôm kính Buôn Ma Thuột Tuketop.com giao hàng rất nhanh và sản phẩm tôi rất ưng ý, giá cả phải chăng và được cái mấy anh lắp đặt nhiệt tình lắm, làm việc không kể thời gian …</p>
+				<span>Facebook</span><br><br>
+				
+			</div>
+			<div class=" arrow arrow-1">
+					<svg viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" transform="translate(100, 100) rotate(180) "></path></svg>
+				</div>
+				<div class="arrow arrow-2">
+					<svg viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z"></path></svg>
+				</div>
+
+
+			<div class="sld sld-2">
+				<i class="fa-solid fa-star"></i>
+				<i class="fa-solid fa-star"></i>
+				<i class="fa-solid fa-star"></i>
+				<i class="fa-regular fa-star"></i>
+				<i class="fa-regular fa-star"></i>
+				<p>Làm nhanh lắm mọi người, họ có nhiều mẫu mã rất đẹp, tôi vừa đặt một tủ trưng bày điện thoại rất ưng ý, hình như họ còn cho thuê nữa …</p>
+				<span>Điện thoại di động Hoàng Long</span>
+
+			</div>
+			
+
+			<div class=" sld sld-3">
+				<div class="star-rating">
+					<span></span>
+					<strong class="rating"></strong>
+				</div>
+				<button type="button">
+					<svg viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" transform="translate(100, 100) rotate(180) "></path></svg>
+				</button>
+				<p>Không phải nói gì hơn 2 chữ ngon lành cành đào, mấy ổng người nhỏ nhỏ mà tay chân khéo phải biết. Tôi đã đặt hàng tại Nhôm Kính Tủ Kệ Top nhiều rồi !</p>
+				<span>Hương Giang</span>
+
+			</div>
+			<div class="input">
+				<input type="radio" id="radio1" name="radio-btn">
+				<input type="radio" id="radio2" name="radio-btn">
+				<input type="radio" id="radio3" name="radio-btn">
+			</div>
+		</div> <!--  end slider -->
+
+
+	</div>
+</body>
+</html>
